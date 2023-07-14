a25605df89a02c01a7fb8c5c6908604cac3d957a
Maycon <maycondouglasniculau@gmail.com>
2023-07-14 12:39:03 -0300

    Linkeet

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "/assets (1)/assets/avatar-2.png")
+    img.setAttribute("src", "assets (1)/assets/avatar-2.png")
   } else {
     img.setAttribute("src", "assets (1)/assets/avatar.png")
   }
